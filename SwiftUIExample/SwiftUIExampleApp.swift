2dc04fb673c27695cef46b77d667b04cf27c771f
Tien Dao <tiendao5240@gmail.com>
2022-11-07 10:53:24 -0500

    Initial Commit

---
+++ SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by Tien Dao on 2022-11-07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
