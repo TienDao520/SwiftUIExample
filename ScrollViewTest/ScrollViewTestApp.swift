d4304155f7d02f3835ce8f763805c4850b464dc6
Tien Dao <tiendao5240@gmail.com>
2022-11-14 14:50:30 -0500

    ScrollViewTest: first commit

---
+++ ScrollViewTest/ScrollViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewTestApp.swift
+//  ScrollViewTest
+//
+//  Created by Tien Dao on 2022-11-14.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
